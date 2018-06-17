fa68c84341823c8d7a141ff3b8a381450df33c15
at-quantran2 <quan.tran2@asiantech.vn>
2018-06-17 22:11:09 +0700

    day2

---
+++ PHP2/display.php
@@ -0,0 +1,11 @@
+<?php
+    $name = $_POST['name'];
+    $age = $_POST['age'];
+    // echo 'Name: ' . $name ;
+    // echo '</br>';
+    // echo 'Age: ' . $age . '</br>' ;
+    // echo "{$age}";
+    $currentDate = date('d,m,Y');
+    $s = '0';
+
+    echo  var_dump(empty($s));
